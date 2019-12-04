7e32c8c5310675b9e92eb7cccb0a4d8f47639aca
Joshua <davidjoshua026@gmail.com>
2019-12-04 06:46:41 +0100

    added heroku settings

--- tongue/wsgi.py
+++ tongue/wsgi.py
@@ -11,14 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tongue.settings')
-
-application = get_wsgi_application()
-
-#DEPLOYMENT SETTINGS
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tongue.settings')
+
+# application = get_wsgi_application()
+
+#DEPLOYMENT SETTINGS
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tongue.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
