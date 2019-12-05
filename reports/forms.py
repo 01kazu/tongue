e5d615c49db004ded30e1621380fe09279babb9d
Joshua <davidjoshua026@gmail.com>
2019-12-05 05:48:26 +0100

    Uncommented Auth

--- reports/forms.py
+++ reports/forms.py
@@ -39,7 +39,7 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2',)
 
     
 class ReportForm(forms.ModelForm):
